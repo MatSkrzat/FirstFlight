ecfdb7ac36a6b388aa4fc5588a0a67f49a544d3a
Mateusz Skrzat <mateusz_skrzat@outlook.com>
2022-06-27 17:52:51 +0200

    Debug UI Removed

--- Assets/Scripts/Managers/LevelsManager.cs
+++ Assets/Scripts/Managers/LevelsManager.cs
@@ -1,13 +1,10 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class LevelsManager : MonoBehaviour
 {
     public const int INITIAL_LEVEL = 0;
     public const string LEVEL_NAME = "level";
     public static LevelModel currentLevel = new LevelModel();
-
-    public GameObject UIDebug;
 
     #region STATIC
     public static LevelsManager instance;
@@ -22,12 +19,6 @@ public class LevelsManager : MonoBehaviour
         TreeModulesManager.currentLevelModules = currentLevel.treeModules;
     }
     #endregion
-
-
-    private void Start()
-    {
-        UIDebug.GetComponent<Text>().text = string.Format("{0}/{1}{2}.json", Application.streamingAssetsPath, PathsDictionary.LEVELS, LEVEL_NAME + INITIAL_LEVEL).Replace(" ", "_");
-    }
     public static LevelModel LoadLevel(int levelID)
     {
         string levelName = LEVEL_NAME + levelID;
